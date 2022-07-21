9efcfb2c5d34a8b43a35ffa80365c8b542da5095
2garam <2garamssi@gmail.com>
2022-07-21 21:38:49 +0900

    Factory method 패턴 - 구체적으로 어떤 인스턴스를 만들지는 서브 클래스가 정한다  : 다양한 구현체 (Product)가 있고, 그중에는 특정한 구현체를 만들 수 있는 다양한 팩토리(Creator)를 제공할 수 있다.
     장점 : 기존 코드를 건드리지 않고 새로운 인스턴스를 다른 방법으로 얼마든지 확장이 가능하다 -> product, creator를 Loose Coupling(느슨한 결합)
     단점 : 역할을 나누다보니 Class가 늘어난다
    
     확장에 열려있고 변경에 닫혀있는 개체 지향 원칙 : 기존 코드를 변경하지 않고 기능을 확장할 수 있는지, 없는지
    
     default 메소드 : 구현 클래스, 함수를 만들 수 있는 기능, 해당 인터페이스를 상속 받았을 때 해당 기능을 사용할 수 있게 한다.
    
     * 인터페이스에 추가된 private 메소드

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory_method/_02_after/DefaultShipFactory.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory_method/_02_after/DefaultShipFactory.java
@@ -5,6 +5,7 @@ public abstract class DefaultShipFactory implements ShipFactory {
     @Override
     public void sendEmailTo(String email, Ship ship) {
         System.out.println(ship.getName() + " 다 만들었습니다.");
+        System.out.println(email + " 메일 전송.");
     }
 
 }
